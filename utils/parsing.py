0cf7a8dbc82de06be1d27ede2c63ac54f848525f
boula <leo@LAPTOP-G1A1T1KM>
2024-09-07 22:17:42 +0200

    feat: parsing almost done

--- utils/parsing.py
+++ utils/parsing.py
@@ -1,31 +1,61 @@
+import os
+import csv
 import sys
 
-class ParseData:
-    def __init__(self, filename):
-        self.get_data(filename)
+# def is_row_valid(row1, row2, row_len):
+    
+#     if int(row1) < 0 or int(row2) < 0:
+#         print("CSV data cannot be negative.")
+#         return False
+#     if row1.isnumeric() == False or row2.isnumeric() == False:
+#         print("CSV file must contain only numbers.")
+#         return False
+#     if row_len != 2:
+#         print("CSV lines must contain only 2 rows.")
+#         return False
+#     return True
+
+def get_data(filename):
+    
+    data = []
+    
+    try:
+        open_filename = os.path.abspath(os.path.join("../", filename))
         
-    def get_data(self, filename):
-        try:
-            filename = "../" + filename
-            self.data = open(filename, "r")
-            print("data was properly open\n")
-            print(self.data.read())
-        except:
-            print("No data.csv file found. Please use a csv file.\n")
-            exit(1)
+        with open(open_filename, 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            try:
+                next(reader)
+            except StopIteration:
+                pass
 
-
-#---------------------------------------------
-
-def main():
-    args = sys.argv
-    args_len = len(args)
-
-    if (args_len != 2):
-        print("Usage: python parsing.py [filename.csv].\n")
+            for row in reader:
+                if int(row[0]) < 0 or int(row[1]) < 0:
+                    print("CSV data cannot be negative.")
+                    raise
+                if row[0].isnumeric() == False or row[1].isnumeric() == False:
+                    print("CSV file must contain only numbers.")
+                    raise
+                if len(row) != 2:
+                    print("CSV lines must contain only 2 rows.")
+                    raise
+                
+                data_line = { 'km': row[0], 'price': row[1] }
+                data.append(data_line)
+            
+    except:
+        print("EXIT")
         exit(1)
         
-    linear_regression = ParseData(args[1])
+    for item in data:
+        print(item['km'], "-", item['price'])
+           
+           
+#-------------------------------------------------------------------------
+ 
+def main():
+    args = sys.argv
+    get_data(args[1])
     
 if __name__ == "__main__":
     main()
