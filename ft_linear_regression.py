d2d49e99623379bc45c9125950f0ac159ec009ef
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-10-03 10:21:44 +0200

    feat: add main.py

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -1,11 +1,6 @@
-import sys
 from statistics import mean
-from utils.get_data import get_data
-from utils.errors import print_error
-from utils.create_json_file import create_json_file
 from utils.normalization import data_normalization
 
-g_learning_rate = 0.75
 
 class args:
     KM = 'km'
@@ -52,18 +47,4 @@ class LinearRegression:
         
 #-----------------------------------------------    
         
-def main():
-    args = sys.argv
 
-    if len(args) != 2:
-        print_error("ERROR: usage: python parsing.py [data.csv].")
-        exit(1)
-        
-    data = get_data(args[1])
-    create_json_file()
-    
-    model = LinearRegression(data, g_learning_rate)
-
-    
-if __name__ == "__main__":
-    main()
